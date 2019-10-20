1d63fda28dc4b7a1b3e37c4195ef74ba7bafb475
RamyaPG <53213236+RamyaPG@users.noreply.github.com>
2019-10-20 11:27:15 +0530

    Add files via upload

--- infixtopostfix.c
+++ infixtopostfix.c
@@ -1,57 +1,61 @@
-#include<stdio.h>
-char stack[20];
-int top=-1;
-void push(char x)
-{
-    stack[++top]=x;
-
+#define SIZE 50           
+#include <ctype.h>
+char s[SIZE];
+int top=-1;       
+ 
+push(char elem)
+{                       
+    s[++top]=elem;
 }
+ 
 char pop()
-    if(top==-1)
-	   return -1;
-{
-	else
-	   return stack[top--];
+{                     
+    return(s[top--]);
 }
-int priority(char x)
-{
-    if(x=='(')
-	   return 0;
-	if(x=='+'||x=='-')
-	   return 1;
-	if(x=='*'||x=='/')
-	   return 2;
+ 
+int pr(char elem)
+{                 
+    switch(elem)
+    {
+    case '#': return 0;
+    case '(': return 1;
+    case '+':
+    case '-': return 2;
+    case '*':
+    case '/': return 3;
+    }
 }
-int main()
-{
-    char exp[20];
-	char *e,x;
-	printf("Enter the expression:");
-	scanf("%s",&exp);
-	e=exp;
-	while(*e!='\0')
-	{
-	    if(isalnum(*e))
-		   printf("%c",*e);
-		else if(*e=='(')
-		   push(*e);
-		else if(*e==')')
-		{
-		    while((x=pop())!='(')
-			    printf("%c",x);
-		}
-		else
-		{
-		    while(priority(stack[top])>=priority(*e))
-			     printf("%c",pop());
-			push(*e);
-		}
-		e++;
-	}
-	while(top!=-1)
-	{
-	   printf("%c",pop());
-	}
+ 
+ int main()
+{                         
+    char infx[50],pofx[50],ch,elem;
+    int i=0,k=0;
+    printf("\n\nRead the Infix Expression ? ");
+    scanf("%s",infx);
+    push('#');
+    while( (ch=infx[i++]) != '\0')
+    {
+        if( ch == '(') push(ch);
+        else
+            if(isalnum(ch)) pofx[k++]=ch;
+            else
+                if( ch == ')')
+                {
+                    while( s[top] != '(')
+                        pofx[k++]=pop();
+                    elem=pop(); 
+                }
+                else
+                {       
+                    while( pr(s[top]) >= pr(ch) )
+                        pofx[k++]=pop();
+                    push(ch);
+                }
+    }
+    while( s[top] != '#')     
+        pofx[k++]=pop();
+    pofx[k]='\0';          
+    printf("\n\nGiven Infix Expn: %s  Postfix Expn: %s\n",infx,pofx);
 	return 0;
 }
 
